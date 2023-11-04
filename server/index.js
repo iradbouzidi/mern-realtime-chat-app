24fdbd825c617a01f96e4fcd5335541b00d45442
iradbouzidi <b.irad1994@gmail.com>
2023-11-04 17:48:22 +0100

    server side auth done

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 
-//const authRoutes = require("./routes/auth.js");
+const authRoutes = require("./routes/auth.js");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-//app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to React Real-Time Chat App Back-End</h1>");
 });
+
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

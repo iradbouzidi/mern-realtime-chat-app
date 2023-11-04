3075d80b63fa488812ead78ad206a2c8f329492c
iradbouzidi <b.irad1994@gmail.com>
2023-11-04 14:46:52 +0100

    add links

--- client/src/index.js
+++ client/src/index.js
@@ -4,3 +4,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// https://www.youtube.com/watch?v=MJzbJQLGehs
+// https://github1s.com/adrianhajdin/project_medical_pager_chat

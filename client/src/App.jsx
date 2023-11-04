2bbbf06939dc51a1abf3868292a2b5ec9bfbe924
iradbouzidi <b.irad1994@gmail.com>
2023-11-04 15:34:14 +0100

    client-side auth done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,18 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-import { ChannelListContainer, ChannelContainer } from "./components";
+import { ChannelListContainer, ChannelContainer, Auth } from "./components";
 
 import "stream-chat-react/dist/css/index.css";
 import "./App.css";
 
 const apiKey = "hhwu59ttkhv2";
+const authToken = false;
+
 const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
+  if (!authToken) return <Auth />;
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">

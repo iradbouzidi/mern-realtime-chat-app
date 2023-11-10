7ddc834a5db293e278027b6d4b69ac933fead04e
iradbouzidi <b.irad1994@gmail.com>
2023-11-10 18:47:47 +0100

    edit channel done

--- client/src/index.js
+++ client/src/index.js
@@ -5,5 +5,5 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-// https://youtu.be/MJzbJQLGehs
+// https://youtu.be/MJzbJQLGehs 3:06:27
 // https://github1s.com/adrianhajdin/project_medical_pager_chat

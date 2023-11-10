4d3de86ca7b98aa9d9bf2c52bb952ac28a41a29c
iradbouzidi <b.irad1994@gmail.com>
2023-11-10 21:53:02 +0100

    done

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -29,7 +29,7 @@ const Auth = () => {
     const { username, password, phoneNumber, avatarURL } = form;
 
     // const URL = "http://localhost:5000/auth";
-    const URL = "https://react-realtime-chat-app-server.vercel.app/auth";
+    const URL = "https://mern-realtime-chat-app-server.vercel.app/auth";
 
     const {
       data: { token, userId, hashedPassword, fullName },
